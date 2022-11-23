259bbd91710496fd4c7b1c879dd87b33f18bcf61
moveforwardgh <86930329+moveforwardgh@users.noreply.github.com>
2022-11-23 15:47:17 -0300

    Add files via upload

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php
  
-    $to = "diegogalvalisi@gmail.com"; 
+    $to = "wowthemesnet@gmail.com"; 
     $from = $_REQUEST['email']; 
     $name = $_REQUEST['name']; 
     $headers = "From: $from"; 
